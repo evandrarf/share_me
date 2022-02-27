30f5f2655093436d5f5327be5f893dcd47d390dc
evandrarf <evandraraditya049@gmail.com>
2022-02-27 18:36:10 +0700

    Memperbaiki bug pada pin.jsx dan home.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -11,6 +11,7 @@ const Login = () => {
   const navigate = useNavigate();
 
   const responseGoogle = (response) => {
+    console.log(response);
     localStorage.setItem("user", JSON.stringify(response.profileObj));
 
     const { name, googleId, imageUrl } = response.profileObj;
