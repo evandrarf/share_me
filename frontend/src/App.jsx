eb7bda4015a80fad26b7282cc32c35b45a9b2ddc
evandrarf <evandraraditya049@gmail.com>
2022-02-27 16:53:54 +0700

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+
+import Home from "./container/Home";
+import Login from "./components/Login";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
+  );
+};
+
+export default App;
